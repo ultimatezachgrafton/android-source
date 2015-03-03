b755edc4216668862b4ba10a23696e5e91a2ed8d
Zach Grafton <zachgrafton@gmail.com>
2015-03-03 06:27:28 -0800

    initial commit

---
+++ MyApplication/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,16 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Zach on 3/3/2015.
+ */
+public class BloclyActivity {
+    public class BloclyActivity extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_blocly);
+        }
+    }
+}
