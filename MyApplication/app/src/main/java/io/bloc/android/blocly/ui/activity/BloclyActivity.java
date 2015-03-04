b926c8a4df1634e5e3a49f957cac4f9c1f5599e9
Zach Grafton <zachgrafton@gmail.com>
2015-03-03 22:20:18 -0800

    checkpoint 35 almost completed

--- MyApplication/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ MyApplication/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,6 +2,7 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import io.bloc.android.blocly.R;
 
@@ -13,5 +14,8 @@ public class BloclyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
+        Toast.makeText(this,
+                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
+                Toast.LENGTH_LONG).show();
     }
 }
