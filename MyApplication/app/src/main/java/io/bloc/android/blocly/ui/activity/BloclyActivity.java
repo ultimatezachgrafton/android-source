3433e61094ac39d0a6d6c1a59e9700fa2a61ee99
Zach Grafton <zachgrafton@gmail.com>
2015-03-03 20:57:52 -0800

    checkpoint 34 completed

--- MyApplication/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ MyApplication/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,16 +1,17 @@
 package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
 
 /**
  * Created by Zach on 3/3/2015.
  */
-public class BloclyActivity {
-    public class BloclyActivity extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_blocly);
-        }
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
     }
 }
--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,6 +1,18 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
+
+	private String mUrl;
+	private boolean mOpenWhenCompleted;
+	public ImageGetter(String url, boolean openWhenCompleted) {
+		mUrl = url;
+		mOpenWhenCompleted = openWhenCompleted;
+	}
 
 	/*
 	 * ImageGetter
@@ -21,6 +33,27 @@ public class ImageGetter extends Thread {
 
 	@Override
 	public void run() {
+		try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
+			BufferedImage bufferedImage = ImageIO.read(url);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.searator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 		/************************************************
  		 *	ASSIGNMENT:
  		 *	Perform the work found in `Main.java` in this
