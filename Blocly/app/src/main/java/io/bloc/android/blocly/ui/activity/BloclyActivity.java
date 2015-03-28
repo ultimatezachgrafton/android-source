16139c6ea8d5913dc63672e699b7ab51cbe6e9bc
Zach Grafton <zachgrafton@gmail.com>
2015-03-27 18:28:02 -0700

    checkpoint 39 completed

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,31 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.ui.adapter.ItemAdapter;
+
+/**
+ * Created by Zach on 3/25/2015.
+ */
+public class BloclyActivity extends Activity {
+
+    private ItemAdapter itemAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+        itemAdapter = new ItemAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
+    }
+
+}
---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -0,0 +1,125 @@
+package io.bloc.android.blocly.ui.adapter;
+
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import io.bloc.android.blocly.BloclyApplication;
+import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.api.DataSource;
+import io.bloc.android.blocly.api.model.RssFeed;
+import io.bloc.android.blocly.api.model.RssItem;
+
+/**
+ * Created by Zach on 3/25/2015.
+ */
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterViewHolder> {
+
+    private static String TAG = ItemAdapter.class.getSimpleName();
+
+
+    @Override
+    public ItemAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int index) {
+        View inflate = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rss_item, viewGroup, false);
+        return new ItemAdapterViewHolder(inflate);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ItemAdapterViewHolder itemAdapterViewHolder, int index) {
+        DataSource sharedDataSource = BloclyApplication.getSharedDataSource();
+        itemAdapterViewHolder.update(sharedDataSource.getFeeds().get(0), sharedDataSource.getItems().get(index));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return BloclyApplication.getSharedDataSource().getItems().size();
+    }
+
+
+    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener, View.OnClickListener {
+
+        TextView title;
+        TextView feed;
+        TextView content;
+        View headerWrapper;
+        ImageView headerImage;
+        // #6
+        RssItem rssItem;
+
+        public ItemAdapterViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.tv_rss_item_title);
+            feed = (TextView) itemView.findViewById(R.id.tv_rss_item_feed_title);
+            content = (TextView) itemView.findViewById(R.id.tv_rss_item_content);
+            headerWrapper = itemView.findViewById(R.id.fl_rss_item_image_header);
+            headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
+            itemView.setOnClickListener(this);
+        }
+
+        void update(RssFeed rssFeed, RssItem rssItem) {
+            this.rssItem = rssItem;
+            feed.setText(rssFeed.getTitle());
+            title.setText(rssItem.getTitle());
+            content.setText(rssItem.getDescription());
+            if (rssItem.getImageUrl() != null) {
+// #8
+                headerWrapper.setVisibility(View.VISIBLE);
+                headerImage.setVisibility(View.INVISIBLE);
+                ImageLoader.getInstance().loadImage(rssItem.getImageUrl(), this);
+            } else {
+// #9
+                headerWrapper.setVisibility(View.GONE);
+            }
+        }
+
+        /*
+          * ImageLoadingListener
+          */
+
+        @Override
+        public void onLoadingStarted(String imageUri, View view) {}
+
+        @Override
+        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+            Log.e(TAG, "onLoadingFailed: " + failReason.toString() + " for URL: " + imageUri);
+        }
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+// #10
+            if (imageUri.equals(rssItem.getImageUrl())) {
+                headerImage.setImageBitmap(loadedImage);
+                headerImage.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onLoadingCancelled(String imageUri, View view) {
+            // Attempt a retry
+            ImageLoader.getInstance().loadImage(imageUri, this);
+        }
+
+        /*
+          * OnClickListener
+          */
+
+        // #7
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), rssItem.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
